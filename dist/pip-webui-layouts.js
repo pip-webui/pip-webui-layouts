e49fda6e165b29f417bae2bbd8a04743ac1c1d79
Sergey Seroukhov <seroukhov@gmail.com>
2016-10-09 23:33:27 -0500

    Updated gulp files

--- dist/pip-webui-layouts.js
+++ dist/pip-webui-layouts.js
@@ -16,6 +16,87 @@
 })();
 
 /**
+ * @file Dialog layout
+ * @copyright Digital Living Software Corp. 2014-2015
+ */
+
+/* global angular */
+
+(function () {
+    'use strict';
+
+    var thisModule = angular.module('pipLayout.Dialog', []);
+
+    thisModule.directive('pipDialog', function() {
+        return {
+           restrict: 'EA',
+           controller: 'pipDialogController'
+        };
+    });
+
+    thisModule.controller('pipDialogController', 
+        ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
+            var $window = $(window);
+
+            // Add class to the element
+            $element.addClass('pip-dialog');
+
+            // Resize every time window is resized
+            $scope.$on('pipWindowResized', resize);
+
+            $scope.$on('pipResizeLayout', resize);
+
+
+            // Resize right away to avoid flicking
+            resize();
+
+            // Resize the element right away
+            //setTimeout(resize, 0);
+            $window.trigger('resize');
+
+            return;
+
+            //-----------------------
+
+            function resize() {
+                var
+                    maxWidth = $window.width(),
+                    maxHeight = $window.height(),
+                    minWidth = $attrs.minWidth ? Math.floor($attrs.minWidth) : null,
+                    minHeight = $attrs.minHeight ? Math.floor($attrs.minHeight) : null,
+                    width = $attrs.width ? Math.floor($attrs.width) : null,
+                    height = $attrs.height ? Math.floor($attrs.height) : null;
+
+                // Set margin and maximum dimensions
+                var space = maxWidth > 1200 ? 24 : 16;
+                maxWidth -= space * 2;
+                maxHeight -= space * 2;
+
+                // Set minimum dimensions
+                minWidth = minWidth && minWidth < maxWidth ? minWidth : null;
+                minHeight = minHeight && minHeight < maxHeight ? minHeight : null;
+
+                // Set regular dimensions
+                width = width ? Math.min(width, maxWidth) : null;
+                height = height ? Math.min(height, maxHeight) : null;
+
+                // Set dimensions
+                $element.css('max-width', maxWidth ? maxWidth + 'px' : '');
+                $element.css('max-height', maxHeight ? maxHeight + 'px' : '');
+                $element.css('min-width', minWidth ? minWidth + 'px' : '');
+                $element.css('min-height', minHeight ? minHeight + 'px' : '');
+                $element.css('width', width ? width + 'px' : '');
+                $element.css('height', height ? height + 'px' : '');
+
+                // Notify child controls that layout was resized
+                $scope.$broadcast('pipLayoutResized');
+            };
+        }]
+    );
+
+})();
+
+/**
  * @file Card layout
  * @copyright Digital Living Software Corp. 2014-2015
  */
@@ -137,7 +218,7 @@
 })();
 
 /**
- * @file Dialog layout
+ * @file Document layout
  * @copyright Digital Living Software Corp. 2014-2015
  */
 
@@ -146,74 +227,21 @@
 (function () {
     'use strict';
 
-    var thisModule = angular.module('pipLayout.Dialog', []);
+    var thisModule = angular.module('pipLayout.Document', []);
 
-    thisModule.directive('pipDialog', function() {
+    thisModule.directive('pipDocument', function() {
         return {
            restrict: 'EA',
-           controller: 'pipDialogController'
+           controller: 'pipDocumentController'
         };
     });
 
-    thisModule.controller('pipDialogController', 
+    thisModule.controller('pipDocumentController', 
         ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
-            var $window = $(window);
-
             // Add class to the element
-            $element.addClass('pip-dialog');
-
-            // Resize every time window is resized
-            $scope.$on('pipWindowResized', resize);
-
-            $scope.$on('pipResizeLayout', resize);
-
-
-            // Resize right away to avoid flicking
-            resize();
-
-            // Resize the element right away
-            //setTimeout(resize, 0);
-            $window.trigger('resize');
-
-            return;
-
-            //-----------------------
-
-            function resize() {
-                var
-                    maxWidth = $window.width(),
-                    maxHeight = $window.height(),
-                    minWidth = $attrs.minWidth ? Math.floor($attrs.minWidth) : null,
-                    minHeight = $attrs.minHeight ? Math.floor($attrs.minHeight) : null,
-                    width = $attrs.width ? Math.floor($attrs.width) : null,
-                    height = $attrs.height ? Math.floor($attrs.height) : null;
-
-                // Set margin and maximum dimensions
-                var space = maxWidth > 1200 ? 24 : 16;
-                maxWidth -= space * 2;
-                maxHeight -= space * 2;
-
-                // Set minimum dimensions
-                minWidth = minWidth && minWidth < maxWidth ? minWidth : null;
-                minHeight = minHeight && minHeight < maxHeight ? minHeight : null;
-
-                // Set regular dimensions
-                width = width ? Math.min(width, maxWidth) : null;
-                height = height ? Math.min(height, maxHeight) : null;
-
-                // Set dimensions
-                $element.css('max-width', maxWidth ? maxWidth + 'px' : '');
-                $element.css('max-height', maxHeight ? maxHeight + 'px' : '');
-                $element.css('min-width', minWidth ? minWidth + 'px' : '');
-                $element.css('min-height', minHeight ? minHeight + 'px' : '');
-                $element.css('width', width ? width + 'px' : '');
-                $element.css('height', height ? height + 'px' : '');
-
-                // Notify child controls that layout was resized
-                $scope.$broadcast('pipLayoutResized');
-            };
+            $element.addClass('pip-document');
         }]
-    );
+    );    
 
 })();
 
@@ -280,34 +308,6 @@
             }
         }]
     );
-
-})();
-
-/**
- * @file Document layout
- * @copyright Digital Living Software Corp. 2014-2015
- */
-
-/* global angular */
-
-(function () {
-    'use strict';
-
-    var thisModule = angular.module('pipLayout.Document', []);
-
-    thisModule.directive('pipDocument', function() {
-        return {
-           restrict: 'EA',
-           controller: 'pipDocumentController'
-        };
-    });
-
-    thisModule.controller('pipDocumentController', 
-        ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
-            // Add class to the element
-            $element.addClass('pip-document');
-        }]
-    );    
 
 })();
 
