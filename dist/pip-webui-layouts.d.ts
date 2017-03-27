10413a4284803250c005b226dd0ab44162b8259d
Anastas Fonotov <anastasf@gmail.com>
2017-03-27 21:12:54 +0300

    'rebuild'

--- dist/pip-webui-layouts.d.ts
+++ dist/pip-webui-layouts.d.ts
@@ -40,12 +40,6 @@ export interface IAuxPanelProvider extends ng.IServiceProvider {
 }
 
 
-
-
-
-
-
-
 export const MainResizedEvent = "pipMainResized";
 export const LayoutResizedEvent = "pipLayoutResized";
 export class MediaBreakpoints {
@@ -83,6 +77,12 @@ export let MainBreakpointStatuses: MediaBreakpointStatuses;
 
 export function addResizeListener(element: any, listener: any): void;
 export function removeResizeListener(element: any, listener: any): void;
+
+
+
+
+
+
 
 }
 
